5ec929baf956702dda7e0ef2afd6442c8fcc20c7
edenbuskila <edenbuskila16616@gmail.com>
2020-04-07 14:48:05 +0300

    added job

--- venv/app.py
+++ venv/app.py
@@ -4,13 +4,16 @@ from xls_to_json import create_jsons_from_tags , read_excel_sheet
 import json
 from elasticsearch import Elasticsearch
 import uuid
+import schedule
+import time
 
 stt_arabic = {'folder_id' : STT_ARABIC_FOLDER_ID , 'tgt_path' : STT_ARABIC_TGT_PATH, 'name' : 'stt_arabic'}
 stt_farsi  = {'folder_id' : STT_FARSI_FOLDER_ID , 'tgt_path' : STT_FARSI_TGT_PATH, 'name' : 'stt_farsi'}
 literary_arabic = {'folder_id' : LITERARY_ARABIC_FOLDER_ID , 'tgt_path' : LITERARY_ARABIC_TGT_PATH, 'name' : 'literary_arabic'}
 literary_farsi = {'folder_id' : LITERARY_FARSI_FOLDER_ID , 'tgt_path' : LITERARY_FARSI_TGT_PATH, 'name' : 'literary_farsi'}
+schedule.every().day.at("11:00").do(job)
 
-def main():
+def job():
     sagah_list = [stt_arabic, stt_farsi, literary_arabic, literary_farsi]
     tags_jsons = []
     for sagah in sagah_list:
@@ -31,6 +34,11 @@ def main():
         for data in tag_json['data']:
             for tag in data['tags']:
                 es.index(index=sagah, id=uuid.uuid4(), body=tag)
+                
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
